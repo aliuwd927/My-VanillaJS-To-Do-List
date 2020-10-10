8b4e072c1afb1449d873cb61e25de97fe7d254ba
Andy Liu <aliu927@gmail.com>
2020-10-10 01:35:28 -0700

    update

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const addToDo = document.querySelector('.addToDo');
 const inputToDoText = document.querySelector('.inputToDoText');
+const todoList = document.querySelector('todoList');
 
 addToDo.addEventListener('click', addToBody);
 
-function addToBody(event) {
-    event.preventDefault();
+function addToBody(){
 
-    
-    const listOfToDo = [];
-        listOfToDo.push(inputToDoText.value);
-    console.log(listOfToDo);
+  //Create To Do Divs
+  const todoDiv = document.createElement('div');
+  todoDiv.classList.add('todo');
+
+  //Create LI
+  const newTodo = document.createElement('li');
+  const newContent = document.createTextNode('Test');
+  newTodo.classList.add('todo-item');
+
+  //
+  todoDiv.appendChild('newToDo');
 };
