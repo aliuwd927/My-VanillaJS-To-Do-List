0a0ce91baed7f13816b101c9e2fc24acca848d5f
Andy Liu <aliu927@gmail.com>
2020-10-10 22:18:44 -0700

    update

--- index.js
+++ index.js
@@ -5,18 +5,20 @@ const todoList = document.querySelector('.todoList');
 addToDo.addEventListener('click', addToBody);
 
 function addToBody(){
-
   //Create To Do Divs
   const todoDiv = document.createElement('div');
   todoDiv.classList.add("todo");
-
   //Create LI
   const newTodo = document.createElement('li');
   newTodo.innerText = todoInput.value;
   newTodo.classList.add("todo-item");
   todoDiv.appendChild(newTodo);
 
-  
+  //Create Completed Button
+  const completedButton = document.createElement('button');
+  completedButton.innerText = '<i class="fas fa-check"></i>';
+  completedButton.classList.add('completedButton');
+  completedButton.appendChild(completedButton);
 
   //Append to list
   todoList.appendChild(todoDiv);
