8da29b8abad78a388ea344bfd42cfdee6cac3e66
Andy Liu <aliu927@gmail.com>
2020-10-11 23:04:00 -0700

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const todoInput = document.querySelector('.inputToDoText');
 const addToDo = document.querySelector('.addToDo');
 const todoList = document.querySelector('.todoList');
-const filterToDo = document.querySelector('.dropDown');
+const filterToDo = document.querySelector('.filter-todo');
 
 //Uses the created trash button from addToBody function to delete the entry todo list
 
@@ -9,7 +9,7 @@ const filterToDo = document.querySelector('.dropDown');
 
 addToDo.addEventListener('click', addToBody);
 todoList.addEventListener('click', removeFromBody);
-filterToDo.addEventListener('click', filterItems);
+//filterToDo.addEventListener('click', filterItems);
 
 function addToBody(e){
   e.preventDefault();
@@ -45,30 +45,52 @@ function removeFromBody(e){
   const item = e.target;
     if(item.classList[0] === 'trash-btn'){
       const todo = item.parentElement;
+      todo.classList.add('fall');
       todo.remove();
     };
 
-    if(item.classList[0] === 'complete-btn'){
-      const todo = item.parentElement;
-      todo.classList.toggle('completed');
-    }
+  if (item.classList[0] === "complete-btn") {
+    const todo = item.parentElement;
+    todo.classList.toggle("completed");
+    console.log(todo);
+  }
   
 
 };
 
-function filterItems(e){
+
+
+
+
+
+
+
+
+
+/*
+function filterItems(e) {
   const todos = todoList.childNodes;
-  todos.forEach(function(){
-    switch(e.target.value){
+  todos.forEach(function(todo) {
+    switch (e.target.value) {
       case "all":
-        todo.style.display ='flex';
+        todo.style.display = "flex";
         break;
       case "completed":
-        if(todo.classList.contains('completed')){
-          todo.style.display = 'flex';
-        } else{
-          todo.style.display= 'none';
+        if (todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+          console.log('you have selected ');
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+      case "uncompleted":
+        if (!todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
         }
     }
   });
-};
+}
+
+*/
